883daf4bbedc00a67265ea4fcb70e2728087e66c
Arnur <arnursabet@gmail.com>
2020-04-27 15:20:37 -0400

    admin modific

--- public/admin.php
+++ public/admin.php
@@ -1,17 +1,42 @@
-<html>
-  <head>
-    <title> Administrator Page </title>
- <head>
-  <style>
-    body {
-   font-family:arial;
-   background-color : lightblue;
-   color:red;
-  };
- </style>
+<?php
+include("templates/header.php");
+?>
 <body><br>
  <h1 align="center">
   Welcome To Administrator Page 
   </h1>
+
+
+<nav class="navbar bg-light">
+
+<!-- Links -->
+<ul class="navbar-nav">
+  <li class="nav-item">
+    <a class="nav-link" href="">Add a document</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="">Search document and check status</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="">Add new reader</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="">Print branch information</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="">Print top 10 most frequent borrowers</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="">Reserved documents</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="">Document Reserve</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="">Document's by publisher'</a>
+  </li>
+</ul>
+
+</nav>
  </body>
  </html>
